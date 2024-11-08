9c4f4fb0dc04e45c034d9c5b385270890d484149
(Student  C00310148) Troy Alatise <C00310148@setu.ie>
2024-11-08 12:57:41 +0000

    readdedZ

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -25,12 +25,12 @@ int main()
 	std::cout << "Pick a letter from A to A or Z to Y" << std::endl; // change this line each iteration
 	std::cin >> letter;
 	if (letter < 'A' || letter > 'Z') // not a capital letter
-	{ 
+	{
 		if (letter < 'a' || letter > 'y') // not a lower case letter
 		{
 			std::cout << "You need to select a letter from A to Y" << std::endl;
-			return 0; 
-		}		
+			return 0;
+		}
 	}
 	if (letter == 'a' || letter == 'A') // written by Pete
 	{
@@ -54,7 +54,7 @@ int main()
 		{
 			std::cout << "Australia, Antarctica, Aruba, Anguilla Leeward Islands, Antigua and Barbuda, American Samoa" << std::endl;
 		}
-	}	
+	}
 	if (letter == 'b' || letter == 'B')
 	{
 		if (region == 1)
@@ -78,10 +78,31 @@ int main()
 			std::cout << "Bahrain Arabian Peninsula" << std::endl;
 		}
 	}
+}
 	if (letter == 'z' || letter == 'Z') // written by Pete
 	{
-
+		if	(region == 1)
+		{
+			std::cout << "There are no Zs in the Americas" << std::endl; // default negative response
+		}
+		if (region == 2)
+		{
+			std::cout << "There are no Zs in Europe" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "Zimbabwe, Zambia" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "There are no Zs in Aisa" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "There are no Zs in rest of the world" << std::endl;
+		}
 	}
+
 
 	if (letter == 'y' || letter == 'Y') // written by Troy
 	{
@@ -107,7 +128,11 @@ int main()
 		}
 
 	}
-	
+
+}
+
+
+}
 	
 	
 	return 1;
